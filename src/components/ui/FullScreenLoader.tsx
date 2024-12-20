bcf66eb1dd642fdd072b068f5e8942be2d62f9cb
Angel Garcia <adjcg15@gmail.com>
2024-12-20 17:28:05 -0600

    feat(full screen loader): creates component structure and animation

---
+++ src/components/ui/FullScreenLoader.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import React from "react";
+
+export const FullScreenLoader = () => {
+  return (
+    <div style={{ height: "100vh"}} className="flex justify-center items-center w-screen">
+      <Image src="/plain-logo.svg" alt="Logo de la tienda sin texto" width={70} height={70} className="rotate"/>
+    </div>
+  );
+}
