f91e429665c4a0654e21bc787f5ab5bc289a85ba
Angel Garcia <adjcg15@gmail.com>
2024-12-20 22:32:01 -0600

    feat(icon button): implements icon button component

---
+++ src/components/buttons/IconButton.tsx
@@ -0,0 +1,16 @@
+import { ButtonHTMLAttributes, FC, ReactNode } from "react";
+
+type IconButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  children?: ReactNode;
+};
+
+export const IconButton: FC<IconButtonProps> = (props) => {
+  return (
+    <button
+      {...props}
+      className={`bg-blue-600 hover:bg-blue-700 p-3 rounded-full text-white font-medium ${props.className} text-2xl`}
+    >
+      {props.children}
+    </button>
+  );
+}
