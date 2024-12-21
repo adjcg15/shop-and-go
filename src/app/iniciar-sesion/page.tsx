6391a26ec6bad22baf02b78ee39f24822c2a6c9c
Angel Garcia <adjcg15@gmail.com>
2024-12-21 10:15:59 -0600

    fix(login page): changes page structure to remove use client directive and allow metadata in page

--- src/app/iniciar-sesion/page.tsx
+++ src/app/iniciar-sesion/page.tsx
@@ -1,22 +1,16 @@
-"use client";
-import Image from "next/image";
 import Link from "next/link";
-import { UserTypeSelector } from "./_components/UserTypeSelector";
-import { LoginForm } from "./_components/LoginForm";
-import { useState } from "react";
+import { FormWrapper } from "./_components/FormWrapper";
+import { Metadata } from "next";
 
-export default function LoginPage() {
-  const [isEmployee, setIsEmployee] = useState(false);
+export const metadata: Metadata = {
+  title: "Inicio de sesión para clientes y empleados"
+};
+
+export default function Login() {
 
   return (
-    <div className="my-20 mx-auto max-w-md px-3">
-      <header className="flex flex-col items-center">
-        <Image src="logo-with-name.svg" alt="Logo de SHop&Go con nombre incluido" width={150} height={150}/>
-        <UserTypeSelector isEmployee={isEmployee} setIsEmployee={setIsEmployee}/>
-      </header>
-      <main>
-        <LoginForm isUserEmployee={isEmployee}/>
-      </main>
+    <div className="my-20 mx-auto max-w-md px-5">
+      <FormWrapper />
 
       <footer className="flex justify-center mt-8">
         <p>¿Eres nuevo? <Link href="crear-cuenta">Regístrate aquí</Link></p>
