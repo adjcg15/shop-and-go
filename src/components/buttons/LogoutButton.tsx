890bed271f82aaed44f5498344ba37aa5b5f5118
Angel Garcia <adjcg15@gmail.com>
2024-12-21 12:28:31 -0600

    feat(logout button): extends functionality to accept button parameters

--- src/components/buttons/LogoutButton.tsx
+++ src/components/buttons/LogoutButton.tsx
@@ -1,14 +1,21 @@
 "use client";
-import { useContext } from "react";
+import { ButtonHTMLAttributes, FC, useContext } from "react";
 import { IconButton } from "./IconButton";
 import { MdLogout } from "react-icons/md";
 import AuthContext from "@/contexts/auth/context";
 
-export const LogoutButton = () => {
+type LogoutButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+
+export const LogoutButton:FC<LogoutButtonProps> = (props) => {
   const { logout } = useContext(AuthContext);
 
   return (
-    <IconButton onClick={logout} className="bg-red-600 hover:bg-red-700" title="Cerrar sesión">
+    <IconButton 
+      {...props}
+      onClick={logout}
+      className={`bg-red-600 hover:bg-red-700 ${props.className}`} 
+      title="Cerrar sesión"
+    >
       <MdLogout/>
     </IconButton>
   );
