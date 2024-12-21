58a803eb0b92a80b45b1f93fc640f5db913adaf9
Angel Garcia <adjcg15@gmail.com>
2024-12-20 21:12:29 -0600

    feat(main page): creates provisional link to login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 export default function DeliveryAddressSelectionPage() {
   return (
     <main>
       <h1>Selección de dirección de entrega: Próximamente</h1>
+      <Link href="/iniciar-sesion">Iniciar sesión</Link>
     </main>
   );
 }
