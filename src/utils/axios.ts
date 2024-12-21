9138352baa9282473db9d20cba43f7a3d4cb1f19
Angel Garcia <adjcg15@gmail.com>
2024-12-20 21:18:18 -0600

    feat(utils): implements middleware in api interface to include jwt in all requests

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,15 +1,27 @@
 import axios from "axios";
+import Cookies from "js-cookie";
 
 const shopAndGoAPI = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
   timeout: 10000
 });
 
+shopAndGoAPI.interceptors.request.use((config) => {
+  const token = Cookies.get("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+}, (error) => {
+  return Promise.reject(error);
+});
+
 shopAndGoAPI.interceptors.response.use(
   (response) => {
     const newToken = response.headers["Set-Authorization"];
     if (newToken) {
-      // TODO: update new token
+      Cookies.set("token", newToken, { expires: 1 });
     }
     return response;
   },
