e35dbe6edbbbfe0a68b60a898121543f3bbabdb3
Angel Garcia <adjcg15@gmail.com>
2024-12-21 11:37:30 -0600

    feat(product catalog): creates basic page structure

---
+++ src/app/(store)/catalogo/page.tsx
@@ -0,0 +1,7 @@
+export default function ProductCatalogPage() {
+  return (
+    <main className="px-3 md:px-12 max-w-screen-2xl mx-auto pt-8">
+      <h1>Página del CATÁLOGO DE PRODUCTOS: Próximamente</h1>
+    </main>
+  );
+}
