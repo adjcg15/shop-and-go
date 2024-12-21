4c588b549d90d3f2e07b24f733038a1bf2738992
Angel Garcia <adjcg15@gmail.com>
2024-12-20 23:21:37 -0600

    feat(styles): stablishes container styles to stores and products administration pages

--- src/app/empleados/(aministrator)/productos/page.tsx
+++ src/app/empleados/(aministrator)/productos/page.tsx
@@ -1,6 +1,6 @@
 export default function AdminProductsPage() {
   return (
-    <main>
+    <main className="px-3 md:px-12 max-w-screen-2xl mx-auto pt-8">
       <h1>Página de administración de PRODUCTOS: Próximamente</h1>
     </main>
   );
--- src/app/empleados/(aministrator)/sucursales/page.tsx
+++ src/app/empleados/(aministrator)/sucursales/page.tsx
@@ -1,6 +1,6 @@
 export default function AdminStoresPage() {
   return (
-    <main>
+    <main className="px-3 md:px-12 max-w-screen-2xl mx-auto pt-8">
       <h1>Página de administración de SUCURSALES: Próximamente</h1>
     </main>
   );
