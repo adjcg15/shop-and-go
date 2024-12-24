18e329ba5d8e523b91650744f07fb951ba60e03b
Angel Garcia <adjcg15@gmail.com>
2024-12-23 20:31:41 -0600

    feat(catalog page): defines basic page structure, including sidebar with delivery address and categories filters

--- src/app/(store)/catalogo/page.tsx
+++ src/app/(store)/catalogo/page.tsx
@@ -1,7 +1,24 @@
-export default function ProductCatalogPage() {
+import { getProductCategories } from "@/utils/api/products";
+import { Metadata } from "next";
+import { ProductCategoriesList } from "./_components/ProductCategoriesList";
+import { DeliveryAddress } from "./_components/DeliveryAddress";
+
+export const metadata: Metadata = {
+  title: "Catálogo de productos"
+};
+
+export default async function ProductCatalogPage() {
+  const { productCategories } = await getProductCategories();
+  
   return (
-    <main className="px-3 md:px-12 max-w-screen-2xl mx-auto pt-8">
-      <h1>Página del CATÁLOGO DE PRODUCTOS: Próximamente</h1>
-    </main>
+    <div className="px-3 md:px-12 max-w-screen-2xl mx-auto pt-8 md:grid grid-cols-4 gap-5">
+      <aside>
+        <DeliveryAddress />
+        <ProductCategoriesList productCategories={productCategories}/>
+      </aside>
+      <main className="col-span-3">
+        <h1>Página del CATÁLOGO DE PRODUCTOS: Próximamente</h1>
+      </main>
+    </div>
   );
 }
