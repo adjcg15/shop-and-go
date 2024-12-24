600e2e7b6dae2c523d193c1e08d58347f76ec09e
Angel Garcia <adjcg15@gmail.com>
2024-12-23 23:25:15 -0600

    fix(product categories list): changes component structure to create default product category in constants file and assumes that this is included by default in productCategories prop

--- src/app/(store)/catalogo/_components/ProductCategoriesList.tsx
+++ src/app/(store)/catalogo/_components/ProductCategoriesList.tsx
@@ -1,35 +1,27 @@
 "use client";
 import { ProductCategory } from "@/types/types/model/products";
+import { DEFAULT_PRODUCT_CATEGORY } from "@/utils/constants";
 import { useSearchParams, useRouter } from "next/navigation";
-import { ChangeEvent, FC, useEffect, useMemo, useState } from "react";
+import { ChangeEvent, FC, useEffect, useState } from "react";
 
 type ProductCategoriesListProps = {
   productCategories: ProductCategory[];
 };
 
-const DEFAULT_CATEGORY: ProductCategory = {
-  id: 0,
-  name: "Todos los productos"
-};
-
 export const ProductCategoriesList:FC<ProductCategoriesListProps> = ({ productCategories }) => {
   const router = useRouter();
   const searchParams = useSearchParams();
-  const [selectedCategoryId, setSelectedCategoryId] = useState(DEFAULT_CATEGORY.id);
-  
-  const productCategoriesWithDefault = useMemo(
-    () => [DEFAULT_CATEGORY, ...productCategories], [productCategories]
-  );
+  const [selectedCategoryId, setSelectedCategoryId] = useState(DEFAULT_PRODUCT_CATEGORY.id);
   
   const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
     const categoryId = Number(event.target.value);
-    const categoryExists = productCategoriesWithDefault.some(
+    const categoryExists = productCategories.some(
       (category) => category.id === categoryId
     );
 
     const params = new URLSearchParams(searchParams);
 
-    if(categoryExists && categoryId !== DEFAULT_CATEGORY.id) {
+    if(categoryExists && categoryId !== DEFAULT_PRODUCT_CATEGORY.id) {
       params.set("filtroDeCategoria", String(categoryId));
     } else {
       params.delete("filtroDeCategoria");
@@ -41,22 +33,22 @@ export const ProductCategoriesList:FC<ProductCategoriesListProps> = ({ productCa
   useEffect(() => {
     const activeCategoryFilter = Number(searchParams.get("filtroDeCategoria"));
 
-    const categoryExists = productCategoriesWithDefault.some(
+    const categoryExists = productCategories.some(
       (category) => category.id === activeCategoryFilter
     );
 
-    if(categoryExists && activeCategoryFilter !== DEFAULT_CATEGORY.id) {
+    if(categoryExists && activeCategoryFilter !== DEFAULT_PRODUCT_CATEGORY.id) {
       setSelectedCategoryId(activeCategoryFilter);
     } else {
-      setSelectedCategoryId(DEFAULT_CATEGORY.id);
+      setSelectedCategoryId(DEFAULT_PRODUCT_CATEGORY.id);
     }
-  }, [searchParams, productCategoriesWithDefault, router]);
+  }, [searchParams, productCategories, router]);
 
   return (
     <fieldset>
       <legend className="font-bold mb-2">Categorías</legend>
       {
-        productCategoriesWithDefault.map(category => (
+        productCategories.map(category => (
           <div key={category.id}>
             <label className="font-medium text-base inline-flex items-center mb-2 cursor-pointer">
               <input 
