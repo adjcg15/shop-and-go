ec098507ff66a63ba8ecca119619985661dbc299
Angel Garcia <adjcg15@gmail.com>
2024-12-26 00:03:27 -0600

    feat(catalog page): implements responsive design to categories filters section

--- src/app/(store)/catalogo/_components/ProductCategoriesList.tsx
+++ src/app/(store)/catalogo/_components/ProductCategoriesList.tsx
@@ -3,6 +3,7 @@ import { ProductCategory } from "@/types/types/model/products";
 import { DEFAULT_PRODUCT_CATEGORY } from "@/utils/constants";
 import { useSearchParams, useRouter } from "next/navigation";
 import { ChangeEvent, FC, useEffect, useState } from "react";
+import { MdFilterList } from "react-icons/md";
 
 type ProductCategoriesListProps = {
   productCategories: ProductCategory[];
@@ -12,6 +13,7 @@ export const ProductCategoriesList:FC<ProductCategoriesListProps> = ({ productCa
   const router = useRouter();
   const searchParams = useSearchParams();
   const [selectedCategoryId, setSelectedCategoryId] = useState(DEFAULT_PRODUCT_CATEGORY.id);
+  const [isVisible, setIsVisible] = useState(false);
   
   const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
     const categoryId = Number(event.target.value);
@@ -46,24 +48,33 @@ export const ProductCategoriesList:FC<ProductCategoriesListProps> = ({ productCa
 
   return (
     <fieldset>
-      <legend className="font-bold mb-2">Categorías</legend>
-      {
-        productCategories.map(category => (
-          <div key={category.id}>
-            <label className="font-medium text-base inline-flex items-center mb-2 cursor-pointer">
-              <input 
-                name="category" 
-                value={category.id} 
-                type="radio" 
-                className="mr-2" 
-                onChange={handleInputChange} 
-                checked={selectedCategoryId === category.id}
-              /> 
-              {category.name}
-            </label>
-          </div>
-        ))
-      }
+      <legend onClick={() => setIsVisible((prevValue) => !prevValue)} className="bg-white py-3 px-5 md:p-0 rounded-full md:rounded-none border border-gray-300 font-medium md:border-none md:font-bold md:mb-2 flex items-center">
+        <span className="md:hidden inline-flex items-center mr-1">
+          <MdFilterList size={20} className="mr-2"/> 
+          Filtrar Por 
+        </span> 
+        <span>Categorías</span>
+      </legend>
+      <ul className={`absolute w-full p-3 bg-gray-50 md:bg-transparent -mx-3 md:relative md:p-0 rounded-lg md:rounded-none ${isVisible ? "block" : "hidden"} md:block z-10 border border-gray-300 md:border-none max-h-60 md:max-h-none overflow-y-auto`}>
+        {
+          productCategories.map(category => (
+            <li key={category.id}>
+              <label className="font-medium text-base flex md:inline-flex items-center py-1 md:py-0 mb-2 cursor-pointer" onClick={() => setIsVisible(false)}>
+                <input
+                  name="category" 
+                  value={category.id} 
+                  type="radio" 
+                  className="mr-2 flex-shrink-0" 
+                  onChange={handleInputChange} 
+                  checked={selectedCategoryId === category.id}
+                /> 
+                {category.name}
+              </label>
+            </li>
+          ))
+        }
+      </ul>
+
     </fieldset>
   );
 }
