3461df03b57123f055df5ee6f03107bd59b116f7
Angel Garcia <adjcg15@gmail.com>
2024-12-26 00:50:28 -0600

    docs(home page): documents hint about feedback to user

--- src/app/(store)/page.tsx
+++ src/app/(store)/page.tsx
@@ -1,5 +1,8 @@
 export default function DeliveryAddressSelectionPage() {
   return (
+    // TODO: implement alert when user is redirected here from /catalog. 
+    // Use notify util, useParam hook accessing to "redirigidoDesde" param and nearestStore.error  
+    // value from store context to give feedback to user
     <main className="px-3 md:px-12 max-w-screen-2xl mx-auto pt-8">
       <h1>Selección de dirección de entrega: Próximamente</h1>
     </main>
