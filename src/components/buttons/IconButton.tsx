fd36a9c147a93c8f4c28e5d23a5c8a69a6d466af
Angel Garcia <adjcg15@gmail.com>
2024-12-28 15:59:30 -0600

    fix(IconButton.tsx): changes styles to take into account disabled visual state

--- src/components/buttons/IconButton.tsx
+++ src/components/buttons/IconButton.tsx
@@ -8,7 +8,7 @@ export const IconButton: FC<IconButtonProps> = (props) => {
   return (
     <button
       {...props}
-      className={`bg-blue-600 hover:bg-blue-700 p-3 rounded-full text-white font-medium ${props.className} text-2xl`}
+      className={`bg-blue-600 hover:bg-blue-700 disabled:bg-gray-300 p-3 rounded-full text-white font-medium ${props.className} text-2xl`}
     >
       {props.children}
     </button>
