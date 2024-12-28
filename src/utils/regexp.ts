f4705553f8bbf9ed5b8299a4aee39666309ffe7a
Angel Garcia <adjcg15@gmail.com>
2024-12-28 16:49:37 -0600

    fix(regexp): stablishes maximum char length category name format

--- src/utils/regexp.ts
+++ src/utils/regexp.ts
@@ -6,7 +6,7 @@ const PHONE_NUMBER_PATTERN = new RegExp("^[0-9]{10}$");
 
 const SECURE_PASSWORD_PATTER = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[\\d!_\\-@&%$#])[A-Za-z\\d!_\\-@&%$#]{8,}$");
 
-const CATEGORY_NAME_FORMAT = new RegExp("^[a-zA-Z\\s]+$");
+const CATEGORY_NAME_FORMAT = new RegExp("^[a-zA-Z\\s]{1,255}$");
 
 export  {
     EMAIL_PATTERN,
