9dba88ffabe16cbd849c9133ced7e5c676e26092
Angel Garcia <adjcg15@gmail.com>
2024-12-28 17:08:07 -0600

    feat(admin categories page): implements basic structure for CreateCategorySection

---
+++ src/app/empleados/(aministrator)/categorias/_components/CreateCategorySection.tsx
@@ -0,0 +1,9 @@
+import { PrimaryButton } from "@/components/buttons/PrimaryButton";
+
+export const CreateCategorySection = () => {
+  return (
+    <section className="flex justify-end">
+      <PrimaryButton>Nueva categoría</PrimaryButton>
+    </section>
+  );
+}
