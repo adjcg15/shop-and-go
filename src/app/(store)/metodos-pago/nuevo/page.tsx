0dac8b8ac3a0d1f748bc27fff79fded878456073
Rodrigo Aguilar López <zS21013854@estudiantes.uv.mx>
2024-12-28 19:59:02 -0600

    feat(create-payment-method-use-case): implements consult payment use case with getting issuing banks and register payment method

--- src/app/(store)/metodos-pago/nuevo/page.tsx
+++ src/app/(store)/metodos-pago/nuevo/page.tsx
@@ -7,9 +7,9 @@ export const metadata: Metadata = {
 
 export default function Login() {
   return (
-    <div className="my-20 mx-auto max-w-md px-5 pt-8">
+    <div className="my-20 mx-auto w-3/5 lg:w-2/5 px-5 pt-8">
       <header className="flex flex-col items-center">
-        <h1 className="text-2xl sm:text-3xl font-bold text-gray-800">
+        <h1 className="lg:text-3xl md:text-3xl sm:text-2xl font-bold text-gray-800">
           Registro de tarjeta
         </h1>
       </header>
