35c39ec6bfe7de3e1d0696f83dbc498f0cd3962c
Angel Garcia <adjcg15@gmail.com>
2024-12-28 23:59:19 -0600

    fix(regexp): includes spanish language characters in category name format

--- src/utils/regexp.ts
+++ src/utils/regexp.ts
@@ -6,12 +6,12 @@ const PHONE_NUMBER_PATTERN = new RegExp("^[0-9]{10}$");
 
 const SECURE_PASSWORD_PATTER = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[\\d!_\\-@&%$#])[A-Za-z\\d!_\\-@&%$#]{8,}$");
 
-const CATEGORY_NAME_FORMAT = new RegExp("^[a-zA-Z\\s]{1,255}$");
+const CATEGORY_NAME_FORMAT = new RegExp("^[a-zA-ZáéíóúüñÁÉÍÓÚÜÑ\\s]{1,255}$");
 
 export  {
-    EMAIL_PATTERN,
-    PHONE_NUMBER_PATTERN,
-    SECURE_PASSWORD_PATTER,
-    USERNAME_PATTERN,
-    CATEGORY_NAME_FORMAT
+  EMAIL_PATTERN,
+  PHONE_NUMBER_PATTERN,
+  SECURE_PASSWORD_PATTER,
+  USERNAME_PATTERN,
+  CATEGORY_NAME_FORMAT
 };
