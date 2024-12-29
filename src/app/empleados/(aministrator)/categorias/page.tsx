052a79eb2074901a9d14da73a3469630db91de35
Angel Garcia <adjcg15@gmail.com>
2024-12-29 00:05:47 -0600

    fix(admin categories page): full page implementation

--- src/app/empleados/(aministrator)/categorias/page.tsx
+++ src/app/empleados/(aministrator)/categorias/page.tsx
@@ -1,20 +1,14 @@
 import { Metadata } from "next";
-import { CreateCategorySection } from "./_components/CreateCategorySection";
-import { CategoriesList } from "./_components/CategoriesList";
+import { AdminProductCategoriesWrapper } from "./_components/AdminProductCategoriesWrapper";
 
 export const metadata: Metadata = {
   title: "Administración de categorías de productos"
 };
 
-export default function AdminProductsPage() {
+export default function AdminProductCategoriesPage() {
   return (
     <main className="px-3 md:px-12 max-w-screen-2xl mx-auto pt-8 pb-16">
-      <CreateCategorySection/>
-      
-      <section className="mt-6">
-        <h1>Categorías de productos registradas</h1>
-        <CategoriesList/>
-      </section>
+      <AdminProductCategoriesWrapper/>
     </main>
   );
 }
