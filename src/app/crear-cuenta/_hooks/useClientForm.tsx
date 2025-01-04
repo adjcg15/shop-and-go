db5cf69010cf232979ae70ce34d3eadd99e655f4
engaherr <zS21013858@estudiantes.uv.mx>
2025-01-03 22:13:20 -0600

    fix(ClientForm): moves birthdate validation to a separate utility function

--- src/app/crear-cuenta/_hooks/useClientForm.tsx
+++ src/app/crear-cuenta/_hooks/useClientForm.tsx
@@ -46,35 +46,6 @@ export function useClientForm() {
     const password = watch("password");
     const passwordConfirmation = watch("passwordConfirmation");
 
-    function validateBirthdate(value: string): string | true {
-        const birthDate = new Date(value);
-        const today = new Date();
-    
-        if (birthDate > today) {
-            return "La fecha de nacimiento no puede ser futura.";
-        }
-    
-        let age = today.getFullYear() - birthDate.getFullYear();
-        const monthDifference = today.getMonth() - birthDate.getMonth();
-
-        if (
-            monthDifference < 0 || 
-            (monthDifference === 0 && today.getDate() < birthDate.getDate())
-        ) {
-            age -= 1;
-        }
-    
-        if (age < 18) {
-            return "Debes tener al menos 18 años.";
-        }
-    
-        if (age > 120) {
-            return "Edad inválida. Ingresa una edad válida (menos de 120 años).";
-        }
-    
-        return true;
-    }
-
     const onSubmit: SubmitHandler<ClientForm> = async (
         {
             fullName,
@@ -152,7 +123,6 @@ export function useClientForm() {
         setShowPassword,
         setShowPasswordConfirmation,
         password,
-        passwordConfirmation,
-        validateBirthdate
+        passwordConfirmation
     }
 }
