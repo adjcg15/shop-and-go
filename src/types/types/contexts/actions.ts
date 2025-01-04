c6e31176809989d9cda16abdccf9d18747eb2ff9
engaherr <zS21013858@estudiantes.uv.mx>
2025-01-03 23:21:39 -0600

    feat(auth/actions): adds update client profile action

--- src/types/types/contexts/actions.ts
+++ src/types/types/contexts/actions.ts
@@ -6,13 +6,15 @@ import { Employee, Client } from "../model/users";
 export enum AuthActionTypes {
   START_EMPLOYEE_SESSION = "[Auth] Start employee session",
   START_CLIENT_SESSION = "[Auth] Start client session",
-  END_SESSION = "[Auth] End session"
+  END_SESSION = "[Auth] End session",
+  UPDATE_CLIENT_PROFILE = "[Auth] Update client profile",
 };
 
 type AuthActions = 
   { type: AuthActionTypes.START_EMPLOYEE_SESSION, payload: Employee }
   | { type: AuthActionTypes.START_CLIENT_SESSION, payload: Client }
-  | { type: AuthActionTypes.END_SESSION };
+  | { type: AuthActionTypes.END_SESSION }
+  | { type: AuthActionTypes.UPDATE_CLIENT_PROFILE, payload: Client };
 
 export enum StoreActionTypes {
   SET_DELIVERY_ADDRESS = "[Store] Changing delivery address",
