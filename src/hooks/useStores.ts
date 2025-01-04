53308de70e2304a5e4b3d1184e3ec82a402f31be
Angel Garcia <adjcg15@gmail.com>
2025-01-03 18:07:08 -0600

    feat(hooks): creates useStores hook

---
+++ src/hooks/useStores.ts
@@ -0,0 +1,39 @@
+import { Store } from "@/types/types/model/stores";
+import shopAndGoAPI from "@/utils/axios";
+import { AxiosError, isAxiosError } from "axios";
+import { useCallback, useState } from "react";
+
+type StoresListState = {
+  loading: boolean; 
+  value: Store[]; 
+  error: null | string;
+};
+
+const INITIAL_STORES_LIST_STATE = { loading: true, value: [], error: null };
+
+export function useStores() {
+  const [storesList, setStoresList] = useState<StoresListState>(INITIAL_STORES_LIST_STATE);
+
+  const recoverStores = useCallback(async () => {
+    try {
+      const { data: stores } = await shopAndGoAPI.get<Store[]>("/stores");
+      setStoresList({ loading: false, value: stores, error: null });
+    } catch (error) {
+      let errorLoadingStores =
+        "Estamos teniendo problemas para cargar las sucursales, por favor intente más tarde.";
+
+      if (isAxiosError(error) && error.code === AxiosError.ERR_NETWORK) {
+        errorLoadingStores =
+          "No fue posible establecer una conexión para cargar " +
+          "la lista de sucursales. Verifique que su conexión a Internet es estable o intente más tarde.";
+      }
+
+      setStoresList({ loading: false, value: [], error: errorLoadingStores });
+    }
+  }, []);
+
+  return {
+    recoverStores,
+    storesList
+  };
+}
