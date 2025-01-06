41f2a1f2dbd76e3bfff87d8f52e3f6723bf38002
Angel Garcia <adjcg15@gmail.com>
2025-01-06 17:06:55 -0600

    feat(models): creates order model

--- src/types/types/model/orders.ts
+++ src/types/types/model/orders.ts
@@ -1,3 +1,6 @@
+import { Address } from "./deliveries";
+import { Client } from "./users";
+
 type Incident = {
     id: number;
     creationDate: string;
@@ -5,6 +8,21 @@ type Incident = {
     idOrder: number;
 };
 
+type Order = {
+    id: number;
+    dateOfPurchase: string;
+    deliveryDate: string | null;
+    idStatus: number;
+    idDeliveryAddress: number;
+    idClient: number;
+    idPaymentMethod: number;
+    idStore: number;
+    idDeliveryMan: number;
+    deliveryAddress?: Address;
+    client?: Client;
+};
+
 export type {
-    Incident
+    Incident,
+    Order
 };
