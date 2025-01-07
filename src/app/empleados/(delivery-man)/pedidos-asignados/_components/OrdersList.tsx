2040f6a87c4020c03cff2fca4f812002a620f379
Angel Garcia <adjcg15@gmail.com>
2025-01-06 23:59:36 -0600

    feat(get orders to deliver page): modifies components to include create incident behaviour

--- src/app/empleados/(delivery-man)/pedidos-asignados/_components/OrdersList.tsx
+++ src/app/empleados/(delivery-man)/pedidos-asignados/_components/OrdersList.tsx
@@ -5,68 +5,88 @@ import { useOrdersToDeliverList } from "../_hooks/useOrdersToDeliverList";
 import { ErrorBanner } from "@/components/ui/ErrorBanner";
 import { OrderToDeliverCard } from "./OrderToDeliverCard";
 import { OrderToDeliverCardSkeleton } from "./OrderToDeliverCardSkeleton";
+import { CreateIncidentModal } from "./CreateIncidentModal";
+import { useCreateIncident } from "../_hooks/useCreateIncident";
 
 export const OrdersList = () => {
   const {
     ordersToDeliver,
-    recoverOrdersToDeliver
+    recoverOrdersToDeliver,
+    onOrderCanceled
   } = useOrdersToDeliverList();
+
+  const {
+    isCreatingIncident,
+    discardIncidentCreation,
+    startIncidentCreation,
+    incidentForm
+  } = useCreateIncident(onOrderCanceled);
 
   useEffect(() => {
     recoverOrdersToDeliver();
   }, [recoverOrdersToDeliver]);
 
   return (
-    <APIProvider apiKey={process.env.NEXT_PUBLIC_MAPS_API_KEY || ""}>
-      {
-        ordersToDeliver.error
-        ? (
-          <ErrorBanner
-            image={{
-              src: "/illustrations/server-error.svg",
-              alt: "Ilustración representativa de un error en un servidor"
-            }}
-            title="¡Problemas técnicos!"
-            message={ordersToDeliver.error}
-          />
-        )
-        : (
-          <ul className="lg:grid mt-3">
-            {
-              ordersToDeliver.loading
-              ? (
-                Array.from({ length: 6 }, (_, index) => (
-                  <li key={index} className="mb-4 lg:mb-0">
-                    <OrderToDeliverCardSkeleton/>
-                  </li>
-                ))
-              )
-              : (
-                ordersToDeliver.value.length > 0
+    <>
+      <APIProvider apiKey={process.env.NEXT_PUBLIC_MAPS_API_KEY || ""}>
+        {
+          ordersToDeliver.error
+          ? (
+            <ErrorBanner
+              image={{
+                src: "/illustrations/server-error.svg",
+                alt: "Ilustración representativa de un error en un servidor"
+              }}
+              title="¡Problemas técnicos!"
+              message={ordersToDeliver.error}
+            />
+          )
+          : (
+            <ul className="lg:grid mt-3">
+              {
+                ordersToDeliver.loading
                 ? (
-                  ordersToDeliver.value.map(order => (
-                    <li className="mb-4 lg:mb-0" key={order.id}>
-                      <OrderToDeliverCard order={order}/>
+                  Array.from({ length: 6 }, (_, index) => (
+                    <li key={index} className="mb-4 lg:mb-0">
+                      <OrderToDeliverCardSkeleton/>
                     </li>
                   ))
                 )
                 : (
-                  <div className="col-span-full">
-                    <ErrorBanner
-                      image={{
-                        src: "/illustrations/empty-cart.svg",
-                        alt: "Imagen representativa de un carrito de compras vacío",
-                      }}
-                      title={"Lista vacía"}
-                      message={`Parece ser que aún no tienes pedidos para entregar.`}
-                    />
-                  </div>
+                  ordersToDeliver.value.length > 0
+                  ? (
+                    ordersToDeliver.value.map(order => (
+                      <li className="mb-4 lg:mb-0" key={order.id}>
+                        <OrderToDeliverCard 
+                          startIncidentCreation={startIncidentCreation}
+                          order={order}
+                        />
+                      </li>
+                    ))
+                  )
+                  : (
+                    <div className="col-span-full">
+                      <ErrorBanner
+                        image={{
+                          src: "/illustrations/empty-cart.svg",
+                          alt: "Imagen representativa de un carrito de compras vacío",
+                        }}
+                        title={"Lista vacía"}
+                        message={`Parece ser que aún no tienes pedidos para entregar.`}
+                      />
+                    </div>
+                  )
                 )
-              )
-            }
-          </ul>
-        )
-      }
-    </APIProvider>
+              }
+            </ul>
+          )
+        }
+      </APIProvider>
+      <CreateIncidentModal 
+        isOpen={isCreatingIncident}
+        handleCloseModal={discardIncidentCreation}
+        formState={incidentForm}
+      />
+    </>
   );
 }
