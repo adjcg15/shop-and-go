c2d2aa725eb5bbbd707caf4d38530c6f55f97082
engaherr <zS21013858@estudiantes.uv.mx>
2025-01-08 02:42:11 -0600

    feat(AddressForm): implements address form with autocomplete and map integration

---
+++ src/app/(store)/_components/AddressForm.tsx
@@ -0,0 +1,72 @@
+import { Address } from "@/types/types/model/deliveries";
+import { FC } from "react";
+import { useAddressForm } from "../_hooks/useAddressForm";
+import { PlaceAutocomplete } from "@/components/inputs/PlaceAutocomplete";
+import { AdvancedMarker, Map, Pin } from "@vis.gl/react-google-maps";
+import { SecondaryButton } from "@/components/buttons/SecondaryButton";
+
+type AddressFormProps = {
+  onSubmitComplete: (address: Address) => void;
+};
+
+export const AddressForm: FC<AddressFormProps> = ({ onSubmitComplete }) => {
+  const {
+    errors,
+    handleSubmit,
+    getValues,
+    handleNewPlaceSelected,
+    isSavingChanges,
+  } = useAddressForm(onSubmitComplete);
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className={`mx-auto max-w-lg px-5 pt-8 pb-16 form-group ${errors.street ? "invalid" : ""} mt-0`}>
+        <label htmlFor="street">Dirección</label>
+        <PlaceAutocomplete
+          id="street"
+          onPlaceSelect={handleNewPlaceSelected}
+          value={getValues("street")}
+          disabled={isSavingChanges}
+          placeholder="Calle, número exterior e interior, colonia"
+        />
+        <p className="error">La dirección no puede estar vacía</p>
+        <div className="border border-gray-300 rounded-lg h-72 overflow-hidden mt-1">
+          <Map
+            style={{ width: "100%", height: "100%" }}
+            center={{ lat: getValues("latitude"), lng: getValues("longitude") }}
+            zoom={17}
+            gestureHandling="none"
+            disableDefaultUI={true}
+            id={`map-creating-id`}
+            mapId={`map-creating-id`}
+            reuseMaps
+            scrollwheel={false}
+            keyboardShortcuts={false}
+          >
+            <AdvancedMarker
+              position={{
+                lat: getValues("latitude"),
+                lng: getValues("longitude"),
+              }}
+            >
+              <Pin
+                background="#f97316"
+                borderColor="transparent"
+                glyphColor="#fed7aa"
+              />
+            </AdvancedMarker>
+          </Map>
+        </div>
+
+        <div className="mt-5 sm:flex justify-end">
+          <SecondaryButton
+            className="block sm:inline-block w-full sm:w-auto mt-3 sm:mt-0"
+            disabled={isSavingChanges}
+          >
+            Agregar dirección
+          </SecondaryButton>
+        </div>
+      </div>
+    </form>
+  );
+};
