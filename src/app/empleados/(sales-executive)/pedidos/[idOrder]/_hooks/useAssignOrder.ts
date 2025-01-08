a6df3abb00bbe56263a5930fa1a157cdecf061f2
Angel Garcia <adjcg15@gmail.com>
2025-01-07 23:25:38 -0600

    feat(assign order page): implements logic to assign order on useAssignOrder hook

--- src/app/empleados/(sales-executive)/pedidos/[idOrder]/_hooks/useAssignOrder.ts
+++ src/app/empleados/(sales-executive)/pedidos/[idOrder]/_hooks/useAssignOrder.ts
@@ -1,46 +1,87 @@
-import { useState } from "react";
+import { CreateOrderToDeliverErrorCodes } from "@/types/enums/error_codes";
+import { NotificationTypes } from "@/types/enums/notifications";
+import { NotificationInfo } from "@/types/types/components/notifications";
+import shopAndGoAPI from "@/utils/axios";
+import { isClientErrorHTTPCode } from "@/utils/http";
+import { notify } from "@/utils/notifications";
+import { AxiosError, isAxiosError } from "axios";
+import { useRouter } from "next/navigation";
+import { useCallback, useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 
-export function useAssignOrder() {
+export function useAssignOrder(idOrder: number) {
+  const router = useRouter();
   const [isProcessingAssignation, setIsProcessingAssignation] = useState(false);
+  const [isConfirmatingAssignation, setIsConfirmatingAssignation] = useState(false);
   const {
     register,
     handleSubmit: submitWrapper,
-    formState: { errors }
+    formState: { errors },
+    getValues
   } = useForm({
     defaultValues: { idDeliveryMan: 0 }
   });
 
-  const onSubmit: SubmitHandler<{ idDeliveryMan: number }> = async ({ idDeliveryMan }) => {
-    console.log(idDeliveryMan);
-    setIsProcessingAssignation(true);
+  const discardDeliveryManAssignation = useCallback(() => {
+    setIsConfirmatingAssignation(false);
+  }, []);
 
-    // try {
-    //   const { data: incidentCreated } = await shopAndGoAPI.post<Incident>("/incidents", { reason, idOrder });
-    //   notify({
-    //     title: "Pedido cancelado",
-    //     message: "El reporte de incidencia fue generado exitosamente",
-    //     type: NotificationTypes.SUCCESS
-    //   });
-    //   onOrderCanceled(incidentCreated.idOrder);
-    //   setIsCreatingIncident(false);
-    // } catch(error) {
-    //   const notificationInfo: NotificationInfo = {
-    //     title: "Error al guardar",
-    //     message: "El sistema no logró guardar el reporte de incidencia, por favor intente en otro momento.",
-    //     type: NotificationTypes.ERROR,
-    //   };
+  const assignDeliveryManToOrder = useCallback(async () => {
+    if(!isProcessingAssignation) {
+      setIsConfirmatingAssignation(true);
 
-    //   if (isAxiosError(error) && error.code === AxiosError.ERR_NETWORK) {
-    //     notificationInfo.message =
-    //       "No fue posible establecer una conexión para guardar " +
-    //       "el reporte de incidencia. Verifique que su conexión a Internet es estable o intente más tarde.";
-    //   }
+      try {
+        const idDeliveryMan = getValues("idDeliveryMan");
+        await shopAndGoAPI.post(
+          `orders/orders-to-deliver`, 
+          { idOrder, idDeliveryMan }
+        );
+        notify({
+          title: "Pedido asignado",
+          message: "El pedido ha sido asignado correctamente al repartidor",
+          type: NotificationTypes.SUCCESS
+        });
+        router.replace("/empleados/pedidos");
+      } catch (error) {
+        const notificationInfo: NotificationInfo = {
+          title: "Error al asignar",
+          message: "No hemos podido asignar el pedido al repartidor, por favor intente en otro momento",
+          type: NotificationTypes.ERROR,
+        };
+  
+        if (
+          isAxiosError(error) &&
+          isClientErrorHTTPCode(Number(error.response?.status))
+        ) {
+          notificationInfo.type = NotificationTypes.WARNING;
+          switch(error.response?.data.errorCode) {
+            case CreateOrderToDeliverErrorCodes.ORDER_NOT_FOUND:
+              notificationInfo.message = "No hemos podido encontrar el pedido, posiblemente ya ha sido asignado";
+              break;
+            case CreateOrderToDeliverErrorCodes.EMPLOYEE_NOT_FOUND:
+            case CreateOrderToDeliverErrorCodes.EMPLOYEE_FULL_OF_WORK:
+              notificationInfo.message = "No ha sido posible asignar el pedido al "
+                + "repartidor. Posiblemente ya ha alcanzado el límite de pedidos asignados.";
+              break;
+          }
+        } else if (
+          isAxiosError(error) &&
+          error.code === AxiosError.ERR_NETWORK
+        ) {
+          notificationInfo.message =
+            "No fue posible establecer una conexión para asignar el pedido " +
+            "al repartidor. Verifique que su conexión a Internet es estable o intente más tarde.";
+        }
+  
+        notify(notificationInfo);
+      } finally {
+        setIsProcessingAssignation(false);
+      }
+    }
+  }, [isProcessingAssignation, getValues, idOrder, router]);
 
-    //   notify(notificationInfo);
-    // } finally {
-    //   setIsSavingIncidentInfo(false);
-    // }
+  const onSubmit: SubmitHandler<{ idDeliveryMan: number }> = async () => {
+    setIsConfirmatingAssignation(true);
   };
   const handleSubmit = submitWrapper(onSubmit);
 
@@ -49,7 +90,11 @@ export function useAssignOrder() {
       register,
       handleSubmit,
       errors,
-      isProcessingAssignation
-    }
+      isProcessingAssignation,
+      getValues
+    },
+    isConfirmatingAssignation,
+    discardDeliveryManAssignation,
+    assignDeliveryManToOrder
   }
 }
