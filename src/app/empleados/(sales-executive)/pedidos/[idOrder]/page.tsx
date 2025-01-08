e97ac5da984382256e434c7827ef2b205c3cc804
Angel Garcia <adjcg15@gmail.com>
2025-01-07 18:18:05 -0600

    feat(get order to assign): creates basic page structure

---
+++ src/app/empleados/(sales-executive)/pedidos/[idOrder]/page.tsx
@@ -0,0 +1,9 @@
+export default function OrderToAssignPage() {
+  return (
+    <main className="px-4 sm:px-8 lg:px-12 max-w-screen-2xl mx-auto pt-8 pb-16">
+      <h1 className="mb-5">Pedidos por validar</h1>
+
+      
+    </main>
+  )
+}
