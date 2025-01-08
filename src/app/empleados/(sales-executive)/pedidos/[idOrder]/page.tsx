12c341deb25031e4c5b42f23f0576df06616999d
Angel Garcia <adjcg15@gmail.com>
2025-01-07 21:39:27 -0600

    feat(order to assign page): creates wrapper to give structure to page and show it on main page

--- src/app/empleados/(sales-executive)/pedidos/[idOrder]/page.tsx
+++ src/app/empleados/(sales-executive)/pedidos/[idOrder]/page.tsx
@@ -1,9 +1,30 @@
-export default function OrderToAssignPage() {
+import { redirect } from "next/navigation";
+import { OrderByIdWrapper } from "./_components/OrderByIdWrapper";
+
+export async function generateMetadata({ params }: { params: Promise<{ idOrder: string }> }) {
+  const { idOrder } = await params;
+  return {
+    title: `Pedido para asignar ${idOrder.padStart(6, "0")}`,
+  };
+}
+
+export default async function OrderToAssignPage({
+  params,
+}: {
+  params: Promise<{ idOrder: string }>
+}) {
+  const { idOrder } = await params;
+  const parsedIdOrder = Number(idOrder);
+
+  if(isNaN(parsedIdOrder)) {
+    redirect("/empleados/pedidos");
+  }
+  
   return (
     <main className="px-4 sm:px-8 lg:px-12 max-w-screen-2xl mx-auto pt-8 pb-16">
       <h1 className="mb-5">Pedidos por validar</h1>
 
-      
+      <OrderByIdWrapper idOrder={parsedIdOrder} />
     </main>
   )
 }
